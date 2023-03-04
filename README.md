5ed1c06c7a04fc85c6d806cc64445fd6f03bd676
luceritopico2020@gmail.com <luceritopico2020@gmail.com>
2023-03-04 08:38:52 -0500

    se agregó /llamada_telefonica

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # llamada_telefonica
 ingresar el tiempo de duración de una llamada telefónica y determinar la cantidad a pagar de acuerdo con lo siguiente: -a: toda llamada que dure 3 minutos tiene un costo de 300 pesos  -b: cada minuto adicional cuesta 50 pesos
+
+# Analisis
+
+# Diseño
+
+### Diagrama de flujo
+![Diagrama de flujo](diagrama.png "diarama de flujo")
---
+++ llamada_telefonica.py
@@ -0,0 +1,19 @@
+#llamada_telefonica
+#Ingresar el tiempo de duración de una llamada telefonica y determinar la cantidad a pagar de acuerdo con lo siguiente:
+#Toda llamada que dure tres minutos o menos tiene un costo de 300 pesos
+#toda llamada con minuto  adicional cuesta 50 pesos de mas.
+
+#input
+dL=int(input("ingrese el tiempo de duración de la llamada: "))
+
+#procesing
+if(dL<=3):
+    CL=300
+else:
+     CL=300+50*dL-3
+
+#output
+print("-------------------------------")
+print("El costo de la llamada en pesos es: " +str(CL))
+print("---------------------------")    
+        
